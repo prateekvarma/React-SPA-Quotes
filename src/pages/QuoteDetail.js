6fac9b437030e3d0c00a1867a6275f0c01515820
Prateek Varma <prtk.varma@gmail.com>
2022-09-27 14:07:57 -0600

    Added a nester route to display comments, taking dynamic params

--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,5 +1,18 @@
+import { useParams, Route } from "react-router-dom";
+import Comments from "../components/comments/Comments";
+
 const QuoteDetail = () => {
-  return <h1>Quote Detail</h1>;
+    const param = useParams();
+    
+  return (
+    <>
+        <h1>Quote Detail</h1>
+        <p>{param.quoteId}</p>
+        <Route path={`/quotes/${param.quoteId}/comments`}>
+            <Comments />
+        </Route>
+    </>
+  )
 };
 
 export default QuoteDetail;
