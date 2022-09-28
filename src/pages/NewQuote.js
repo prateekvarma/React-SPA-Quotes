51e647cfc8188afd31bb9b75b183c32b3af1f81c
Prateek Varma <prtk.varma@gmail.com>
2022-09-28 14:51:37 -0600

    Navigate to a page without clicking, using programmatic navigation with React Router

--- src/pages/NewQuote.js
+++ src/pages/NewQuote.js
@@ -1,8 +1,12 @@
+import { useHistory } from "react-router-dom";
 import QuoteForm from "../components/quotes/QuoteForm";
 
 const NewQuote = () => {
+  const history = useHistory();
+
   const addQuoteHandler = (quoteData) => {
     console.log(quoteData);
+    history.push("/quotes"); //you'll be re-directed to this page
   };
 
   return <QuoteForm onAddQuote={addQuoteHandler} />;
