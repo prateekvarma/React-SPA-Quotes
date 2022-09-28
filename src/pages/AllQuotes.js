1b83018d0bc30857e7dc6da51fd8a88aa6b97ccb
Prateek Varma <prtk.varma@gmail.com>
2022-09-28 12:30:00 -0600

    Added some dummy data to list some quotes

--- src/pages/AllQuotes.js
+++ src/pages/AllQuotes.js
@@ -1,5 +1,13 @@
+import QuoteList from "../components/quotes/QuoteList";
+
+const DUMMY_QUOTES = [
+  { id: "q1", author: "Prateek", text: "Learning React is fun!" },
+  { id: "q2", author: "James", text: "Singing is fun!" },
+  { id: "q3", author: "Kerry", text: "Guitar solo are fun!" },
+];
+
 const AllQuotes = () => {
-  return <h1>All Quotes</h1>;
+  return <QuoteList quotes={DUMMY_QUOTES} />;
 };
 
 export default AllQuotes;
