af24d46d3fd20039ca92fd628c4a476eb6058ebe
Prateek Varma <prtk.varma@gmail.com>
2022-09-28 16:55:11 -0600

    Using useLocation and useHistory for dynamic URL setting, and sorting data

--- src/components/quotes/QuoteList.js
+++ src/components/quotes/QuoteList.js
@@ -1,13 +1,40 @@
 import { Fragment } from 'react';
+import { useHistory, useLocation } from 'react-router-dom';
 
 import QuoteItem from './QuoteItem';
 import classes from './QuoteList.module.css';
 
+const sortQuotes = (quotes, ascending) => {
+  //helper function that sorts quotes by their id. the 'ascending' is a boolean
+  return quotes.sort((quoteA, quoteB) => {
+    if (ascending) {
+      return quoteA.id > quoteB.id ? 1 : -1;
+    } else {
+      return quoteA.id < quoteB.id ? 1 : -1;
+    }
+  })
+}
+
 const QuoteList = (props) => {
+  const history = useHistory();
+  const location = useLocation(); //this hook gives us the info for the current URL location
+
+  const queryParams = new URLSearchParams(location.search); //URLSearchParams is a browser API
+  const isSortingAscending = queryParams.get('sort') === 'asc'; // returns boolean
+
+  const sortedQuotes = sortQuotes(props.quotes, isSortingAscending);
+
+  const sortingHandler = () => {
+    history.push("/quotes?sort=" + (isSortingAscending ? 'desc' : 'asc')); // "/quotes" is the current location. We're just adding some query params to it
+  }
+
   return (
     <Fragment>
+      <div className={classes.sorting}>
+        <button onClick={sortingHandler}>Sort {isSortingAscending ? 'Descending' : 'Ascending'}</button>
+      </div>
       <ul className={classes.list}>
-        {props.quotes.map((quote) => (
+        {sortedQuotes.map((quote) => (
           <QuoteItem
             key={quote.id}
             id={quote.id}
