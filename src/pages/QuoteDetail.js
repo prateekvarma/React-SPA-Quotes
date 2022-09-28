9c604ca6ea370c2d725a114a4fb0307b8cac51df
Prateek Varma <prtk.varma@gmail.com>
2022-09-28 14:17:24 -0600

    Passed another set of dummy data, through components to display individual components

--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,13 +1,24 @@
 import { useParams, Route } from "react-router-dom";
 import Comments from "../components/comments/Comments";
+import HighlightedQuote from "../components/quotes/HighlightedQuote";
+
+const DUMMY_QUOTES = [
+  { id: "q1", author: "Prateek", text: "Learning React is fun!" },
+  { id: "q2", author: "James", text: "Singing is fun!" },
+  { id: "q3", author: "Kerry", text: "Guitar solo are fun!" },
+];
 
 const QuoteDetail = () => {
     const param = useParams();
+
+    const quote = DUMMY_QUOTES.find((quote) => quote.id === param.quoteId);
+    if(!quote) {
+      return <p>No Quote found!</p>
+    }
     
   return (
     <>
-        <h1>Quote Detail</h1>
-        <p>{param.quoteId}</p>
+        <HighlightedQuote text={quote.text} author={quote.author} />
         <Route path={`/quotes/${param.quoteId}/comments`}>
             <Comments />
         </Route>
